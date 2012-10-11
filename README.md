e66cafff990a646429f82ab84d0b65ec427890a9
Shuky Dvir <shuky@tooveo.com>
2012-10-12 00:24:06 +0300

    adding all the necessary tasks to hook pushover to capistrano + updating README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Capistrano::Pushover
 
-TODO: Write a gem description
+This gem adds hooks to capistrano to send push notifications using [Pushover](https://pushover.net/) service.
 
 ## Installation
 
@@ -18,7 +18,24 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+Add to deploy.rb:
+
+	require 'hipchat/capistrano'
+	
+	set :pushover_app_token, 'Your Pushover Application Token'
+	set :pusehover_user_key, 'Your Pushover User Token'
+	
+Now you are ready to recieve notificaion about deploys.
+
+If a deploy fails you will also recieve notification about the failure.
+
+### Who did it?
+
+To determine the user that is currently running the deploy, the capistrano tasks will look for the following:
+
+1. The $PUSHOVER_USER environment variable.
+2. The git user.name var.
+3. The $USER environment variable.
 
 ## Contributing
 
@@ -27,3 +44,11 @@ TODO: Write usage instructions here
 3. Commit your changes (`git commit -am 'Added some feature'`)
 4. Push to the branch (`git push origin my-new-feature`)
 5. Create new Pull Request
+
+## Copyright
+
+Copyright (c) 2012 Shuky Dvir.
+
+**MIT License.**
+
+See LICENSE for details.
--- lib/capistrano-pushover.rb
+++ lib/capistrano-pushover.rb
@@ -1,7 +1,74 @@
 require "capistrano-pushover/version"
+require "rushover"
+
 
 module Capistrano
   module Pushover
-    # Your code goes here...
+    Capistrano::Configuration.instance(:must_exist).load do
+
+      set :pushover_with_migrations, false
+
+      namespace :pushover do
+
+        task :set_user do
+          set :pushover_client, Rushover::Client.new(pushover_app_token)
+          set :pushover_user, Rushover::User.new(pusehover_user_key, pushover_client)
+        end
+
+        task :configure_for_migrations do
+          set :pushover_with_migrations, true
+        end
+
+        task :notify_deploy_started do
+          on_rollback do
+            pushover_user.notify("#{human} cancelled deployment of #{deployment_name} to #{env}.", :title => "#{application} cancelled deployment", :priority => 1)
+          end
+
+          message = "#{human} is deploying #{deployment_name} to #{env}"
+          message << " (with migrations)" if pushover_with_migrations
+          message << "."
+
+          pushover_user.notify(message, :title => "#{application} started deploying", :priority => priority)          
+        end
+
+        task :notify_deploy_finished do
+          pushover_user.notify("#{human} finished deploying #{deployment_name} to #{env}.", :title => "#{application} finished deploying", :priority => priority)
+        end
+
+        def priority
+          fetch(:pushover_priority, 0)
+        end
+
+        def deployment_name
+          if branch
+            "#{application}/#{branch}"
+          else
+            application
+          end
+        end
+
+        def human
+          ENV['PUSHOVER_USER'] ||
+            if (u = %x{git config user.name}.strip) != ""
+              u
+            elsif (u = ENV['USER']) != ""
+              u
+            else
+              "Someone"
+            end
+        end
+
+        def env
+          fetch(:pushover_env, fetch(:rack_env, fetch(:rails_env, "production")))
+        end
+
+        before "pushover:notify_deploy_started", "pushover:set_user"
+        before "deploy:migrations", "pushover:configure_for_migrations"
+        before "deploy:update_code", "pushover:notify_deploy_started"
+        after  "deploy", "pushover:notify_deploy_finished"
+        after  "deploy:migrations", "pushover:notify_deploy_finished"
+      end
+
+    end
   end
 end
